01979361e08361914bebfe1727d88159a5eb6511
Toni-Ty <antonique2479@yahoo.com>
2019-08-31 02:59:44 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,7 +1,7 @@
-class Genre < ActiveRecord::Base
+class CreateGenres < ActiveRecord::Migration[4.2]
   def change
-  create_table :genres do |t|
-    t.string :name
+    create_table :genres do |t|
+      t.string :name
     end
   end
 end
